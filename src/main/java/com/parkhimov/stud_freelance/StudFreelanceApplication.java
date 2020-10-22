2ecbd37d4307c4e799e1847e5dfe013561b372a1
G0yter <goyter012@gmail.com>
2020-10-22 20:20:11 +0300

    Initial commit

---
+++ src/main/java/com/parkhimov/stud_freelance/StudFreelanceApplication.java
@@ -0,0 +1,13 @@
+package com.parkhimov.stud_freelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudFreelanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudFreelanceApplication.class, args);
+	}
+
+}
